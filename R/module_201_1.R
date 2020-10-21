bb56325722c34fc5fcf899323dedff9e5d763533
Paul Brennan <brennanpincardiff@gmail.com>
2020-10-21 21:41:57 +0100

    Adding code for the two Tidy Data examples.

--- R/module_201_1.R
+++ R/module_201_1.R
@@ -5,6 +5,60 @@ library(ggplot2)
 library(tidyr)
 
 
+## ----download and tidy some data---------------------------------------------
+
+# first example - relatively simple
+link <- "https://github.com/brennanpincardiff/RforBiochemists/raw/master/data/Viability_data_example_R201_20200722_for_tidying.xls"
+
+download.file(url=link, destfile="Viability_data_example.xls", mode="wb")
+
+ex_1 <- read_xls("Viability_data_example.xls")
+
+View(ex_1)
+
+# we can use the gather() function to focus on our two columns
+tidy_ex1 <- gather(ex_1[,2:3])
+
+# rename with useful names
+colnames(tidy_ex1) <- c("Treatment", "Viability")
+
+View(tidy_ex1)
+
+# make a plot...
+ggplot(tidy_ex1, aes(Treatment, Viability)) + 
+    geom_bar(stat = "summary", fun = "mean")
+
+# second example - a more obvious change from wide into long format
+link <- "https://github.com/brennanpincardiff/RforBiochemists/raw/master/data/untidy_drug_data_ex_1.csv"
+
+download.file(url=link, destfile="untidy_data_ex_2.csv", mode="wb")
+
+ex_2 <- read.csv("untidy_data_ex_2.csv")
+
+View(ex_2)
+
+# so even the column names are a bit of a challenge...
+colnames(ex_2)
+
+# column names with numbers cause a problem for R. 
+
+# we will use the pivot_longer() function 
+# we exclude the drug concentration colum 
+# and we exclude the NA cells...
+data2 <- pivot_longer(ex_2, -"X.SU_drug_1..M", values_to = "apoptosis",
+                      values_drop_na = TRUE)
+
+# rename 
+colnames(data2) <- c("Conc", "name", "apop")
+
+View(data2)
+
+# make a plot...
+ggplot(data2, aes(x = Conc, y = apop)) + geom_point() +
+    scale_x_log10() 
+
+
+
 ## ----get_data_for_Fig_1b---------------------------------------------------------
 link <- "https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-019-1263-7/MediaObjects/41586_2019_1263_MOESM8_ESM.xlsx"
 
