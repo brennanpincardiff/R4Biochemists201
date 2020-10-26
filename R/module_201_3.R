26718bd7e896a894a14d39e396f7ac2007611baa
Paul Brennan <brennanpincardiff@gmail.com>
2020-10-26 16:43:15 +0000

    Module 3, update Rmd file. Two R files including answer to exercise 2.

---
+++ R/module_201_3.R
@@ -0,0 +1,67 @@
+## ----packages_we_need, message=FALSE---------------------------------------------------------------
+library(readxl)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(ggpubr)
+
+
+## ----download_data_for_Fig_1h----------------------------------------------------------------------
+link <- "https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-019-1263-7/MediaObjects/41586_2019_1263_MOESM8_ESM.xlsx"
+
+# the download.file() function downloads and saves the file with the name given
+download.file(url=link, destfile="file.xlsx", mode="wb")
+
+# get worksheet names 
+sheet_names <- excel_sheets("file.xlsx")
+
+
+## ----test_file_name, echo=FALSE--------------------------------------------------------------------
+# we can add a little test into our data to ensure we have the correct data.
+if (sheet_names[4] == "Figure 1h") {
+print("We wanted Figure 1h so we have the correct file")
+}else {
+print("I don't think we have the correct file, please check your code...")
+}
+
+
+## ----import_data_for_Fig_1h------------------------------------------------------------------------
+data <- read_excel("file.xlsx", sheet_names[4], skip = 1)
+
+
+
+## ----basic_line_graph------------------------------------------------------------------------------
+# simplify column names for ease...
+colnames(data) <- c("thick", "biolum")
+
+# basic dot plot with a fitted line
+ggplot(data, aes(thick, biolum)) +
+    geom_point() +
+    stat_smooth(method = "lm") 
+
+
+## ----more_customised_line_graph--------------------------------------------------------------------
+# more detailed dot plot with a fitted line
+ggplot(data, aes(thick, biolum)) +
+    geom_point() +
+    stat_smooth(method = "lm", se = FALSE, color="black") +   
+  xlab("Thickness (mm)") +   # label x-axis
+  ylab("Bioluminescence") +    # label y-axis
+  ggtitle("Croft et al, 2019, Figure 1h") +  # add a title
+  expand_limits(y=c(0,10), x = c(0,2)) +
+  theme_classic()
+
+
+## ----line_graph_with_stats-------------------------------------------------------------------------
+# stats with ggscatter
+ggscatter(data, x = "thick", y = "biolum",
+    add = "reg.line",  # Add regression line
+    cor.coef = TRUE, # Add correlation coefficient. see ?stat_cor
+    cor.coeff.args = list(method = "spearman"),
+    xlab = "Thickness (mm)",
+    ylab = "Bioluminesence",
+    ylim = c(0, 10),
+    xlim = c(0,2),
+    title = "Croft et al, 2019, Figure 1h"
+)
+
---
+++ R/module_201_3_ans_ex_2.R
@@ -0,0 +1,29 @@
+## ---- exercise_2-----------------------------------------------------------------------------------
+data_2 <- read_excel("file.xlsx", sheet_names[8], skip = 1)
+colnames(data_2) <- c("thick", "express")
+
+# you can plot it in ggplot but you will need to calculate the stats 
+# and add them manually
+ggplot(data_2, aes(thick, express)) +
+    geom_point() +
+    stat_smooth(method = "lm", se = FALSE, color="black") +   
+    xlab("Thickness (mm)") +   # label x-axis
+    ylab("mRNA expression") +    # label y-axis
+    ggtitle("Croft et al, 2019, Figure 1h") +  # add a title
+    scale_y_continuous(limits=c(-4,12), breaks=c(-4,-2,0,2,4,6,8,10,12)) +
+    theme_classic()
+
+
+## ---- more_for_exercise_2--------------------------------------------------------------------------
+# make the plot with ggscatter()
+plot <- ggscatter(data_2, x = "thick", y = "express",
+                  add = "reg.line",  # Add regression line
+                  cor.coef = TRUE, # Add correlation coefficient. see ?stat_cor
+                  cor.coeff.args = list(method = "spearman"),
+                  xlab = "Thickness (mm)",
+                  ylab = "mRNA expression",
+                  xlim = c(0,1.5),
+                  title = "Croft et al, 2019, Figure 1m"  # add a title
+)
+# use scale_y_continuous() to alter the y axis 
+plot + scale_y_continuous(limits=c(-4,12), breaks=c(-4,-2,0,2,4,6,8,10,12))
